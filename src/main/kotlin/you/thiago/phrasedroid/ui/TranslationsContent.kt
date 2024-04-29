e96dd1d86253cf3726939e52a4d6c2b0ca247eba
thiago-you <thiago.youx@gmail.com>
2024-04-29 09:15:08 -0300

    implementing control buttons layout

--- src/main/kotlin/you/thiago/phrasedroid/ui/TranslationsContent.kt
+++ src/main/kotlin/you/thiago/phrasedroid/ui/TranslationsContent.kt
@@ -6,18 +6,54 @@ import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.UIUtil
 import you.thiago.phrasedroid.data.ResourceFile
 import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.FlowLayout
 import java.awt.Font
 import javax.swing.BorderFactory
 import javax.swing.BoxLayout
+import javax.swing.JButton
 import javax.swing.JPanel
 
 class TranslationsContent(translations: List<ResourceFile>) {
 
-    val contentPanel: JBScrollPane = JBScrollPane()
+    val contentPanel: JPanel = JPanel()
 
     init {
-        contentPanel.viewport.add(buildTranslationsContent(translations))
-        contentPanel.border = BorderFactory.createEmptyBorder(40, 20, 80, 20)
+        contentPanel.layout = BoxLayout(contentPanel, BoxLayout.Y_AXIS)
+        contentPanel.border = BorderFactory.createEmptyBorder(40, 0, 0, 0)
+        contentPanel.add(buildControlsPanel())
+        contentPanel.add(buildScrollablePanel(translations))
+    }
+
+    private fun buildControlsPanel(): JPanel {
+        val controlsPanel = JPanel()
+        controlsPanel.layout = BorderLayout()
+        controlsPanel.border = BorderFactory.createEmptyBorder(0, 20, 0, 20)
+
+        val leftButtonPanel = JPanel(FlowLayout(FlowLayout.LEFT))
+        val escapeDataButton = JButton("Escape Data [CDATA]")
+        escapeDataButton.preferredSize = Dimension(220, 40)
+        escapeDataButton.addActionListener { _ -> }
+        leftButtonPanel.add(escapeDataButton)
+
+        val rightButtonPanel = JPanel(FlowLayout(FlowLayout.RIGHT))
+        val confirmTranslationButton = JButton("Apply Translations")
+        confirmTranslationButton.preferredSize = Dimension(220, 40)
+        confirmTranslationButton.addActionListener { _ ->  }
+        rightButtonPanel.add(confirmTranslationButton)
+
+        controlsPanel.add(leftButtonPanel, BorderLayout.WEST)
+        controlsPanel.add(rightButtonPanel, BorderLayout.EAST)
+
+        return controlsPanel
+    }
+
+    private fun buildScrollablePanel(translations: List<ResourceFile>): JBScrollPane {
+        val scrollablePanel = JBScrollPane()
+        scrollablePanel.viewport.add(buildTranslationsContent(translations))
+        scrollablePanel.border = BorderFactory.createEmptyBorder(50, 20, 80, 20)
+
+        return scrollablePanel
     }
 
     private fun buildTranslationsContent(translations: List<ResourceFile>): JPanel {
