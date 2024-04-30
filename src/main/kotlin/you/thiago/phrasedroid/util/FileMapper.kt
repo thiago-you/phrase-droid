53ae9b1cb6fd0ec2463f9e3ff735ab44d83daa69
thiago-you <thiago.youx@gmail.com>
2024-04-30 15:15:33 -0300

    improve resource validation

--- src/main/kotlin/you/thiago/phrasedroid/util/FileMapper.kt
+++ src/main/kotlin/you/thiago/phrasedroid/util/FileMapper.kt
@@ -7,24 +7,10 @@ import you.thiago.phrasedroid.data.Translation
 object FileMapper {
 
     fun getResourceFilesList(translations: List<Translation>): List<ResourceFile> {
-        val sortedList = translations
+        return translations
             .map { mapToResourceFile(it) }
-            .distinctBy { it.filename }
-            .sortedBy { it.locale }
-            .toMutableList()
-
-        val firstItem  = sortedList.find { it.locale == "pt-BR" }
-
-        firstItem?.also {
-            val firstItemIndex = sortedList.indexOf(firstItem)
-
-            if (firstItemIndex != -1) {
-                sortedList.removeAt(firstItemIndex)
-                sortedList.add(0, firstItem)
-            }
-        }
-
-        return sortedList
+            .validateDefaultItems()
+            .sortList()
     }
 
     private fun mapToResourceFile(translation: Translation): ResourceFile {
@@ -76,5 +62,76 @@ object FileMapper {
 
     private fun getTranslationContent(name: String, content: String): String {
         return "<string name=\"%s\">%s</string>".format(name, content)
+    }
+
+    private fun List<ResourceFile>.validateDefaultItems(): List<ResourceFile> {
+        val list = this.toMutableList()
+
+        if (list.firstOrNull { it.locale == "en" } == null) {
+            list.firstOrNull { it.locale in listOf("en-GB", "en-CA") }?.also {
+                list.add(it.copyItem(""))
+            }
+        }
+
+        if (list.firstOrNull { it.locale == "pt" } == null) {
+            list.firstOrNull { it.locale in listOf("pt", "pt-BR", "pt-PT") }?.also {
+                list.add(it.copyItem("-pt"))
+            }
+        }
+
+        if (list.firstOrNull { it.locale == "es" } == null) {
+            list.firstOrNull { it.locale in listOf("es-AR", "es-CL", "es-CO", "es-MX", "es-ES") }?.also {
+                list.add(it.copyItem("-es"))
+            }
+        }
+
+        if (list.firstOrNull { it.locale == "fr" } == null) {
+            list.firstOrNull { it.locale in listOf("fr-FR", "fr-CA") }?.also {
+                list.add(it.copyItem("-fr"))
+            }
+        }
+
+        if (list.firstOrNull { it.locale == "nl" } == null) {
+            list.firstOrNull { it.locale in listOf("nl-NL") }?.also {
+                list.add(it.copyItem("-nl"))
+            }
+        }
+
+        return list
+    }
+
+    private fun List<ResourceFile>.sortList(): List<ResourceFile> {
+        val sortedList = this
+            .distinctBy { it.filename }
+            .sortedBy { it.locale }
+            .toMutableList()
+
+        val firstItem  = sortedList.find { it.locale == "pt-BR" }
+
+        firstItem?.also {
+            val firstItemIndex = sortedList.indexOf(firstItem)
+
+            if (firstItemIndex != -1) {
+                sortedList.removeAt(firstItemIndex)
+                sortedList.add(0, firstItem)
+            }
+        }
+
+        return sortedList
+    }
+
+    private fun ResourceFile.copyItem(suffix: String): ResourceFile {
+        val itemFilename = "values$suffix/strings.xml"
+        val itemFilePath = "/app/src/main/res/${filename}"
+
+        return ResourceFile(
+            filename = itemFilename,
+            filePath = itemFilePath,
+            name = name,
+            locale = locale,
+            translation = translation,
+            sourceTranslation = sourceTranslation,
+            content = content
+        )
     }
 }
