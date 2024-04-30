5f0906bbffe28048896fe7a83672d2345d8672cc
thiago-you <thiago.youx@gmail.com>
2024-04-30 20:32:41 -0300

    implementing settings page

--- src/main/kotlin/you/thiago/phrasedroid/state/AppSettingsComponent.kt
+++ src/main/kotlin/you/thiago/phrasedroid/state/AppSettingsComponent.kt
@@ -6,7 +6,11 @@ import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.FormBuilder
 import org.jetbrains.annotations.NotNull
 import you.thiago.phrasedroid.enums.SettingsEnum
+import java.awt.Component
+import java.awt.Font
+import javax.swing.BorderFactory
 import javax.swing.BoxLayout
+import javax.swing.JEditorPane
 import javax.swing.JPanel
 
 /**
@@ -14,13 +18,18 @@ import javax.swing.JPanel
  */
 class AppSettingsComponent {
 
-    val panel: JPanel
+    val panel: JPanel = JPanel()
 
     private val settingsFilePathField = TextFieldWithBrowseButton()
 
     private val defaultFileRelativePath by lazy { "/%s".format(SettingsEnum.RELATIVE_PATH.value) }
 
     init {
+        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
+        panel.add(buildSettingsInputComponent(), 0)
+    }
+
+    private fun buildSettingsInputComponent(): JPanel {
         settingsFilePathField.toolTipText = defaultFileRelativePath
         settingsFilePathField.textField.text = defaultFileRelativePath
 
@@ -31,18 +40,50 @@ class AppSettingsComponent {
             getFileChooseDescriptor()
         )
 
-        val inputLabel = JBLabel("Set JSON settings file path: ")
+        val inputLabel = JBLabel("Settings file path (JSON): ")
+        inputLabel.font = inputLabel.font.deriveFont(Font.BOLD)
+        inputLabel.border = BorderFactory.createEmptyBorder(0, 0, 0, 0)
+
+        val noteLabel = JBLabel("Note: Plugin default relative path \"$defaultFileRelativePath\"")
+        noteLabel.border = BorderFactory.createEmptyBorder(10, 0, 10, 0)
+
+        val exampleLabel = JBLabel("JSON file template:")
+        exampleLabel.font = exampleLabel.font.deriveFont(Font.BOLD)
+        exampleLabel.border = BorderFactory.createEmptyBorder(30, 0, 10, 0)
+
+        val examplePanel = buildExamplePanel()
+        examplePanel.border = BorderFactory.createEmptyBorder(0, 0, 0, 0)
+
+        noteLabel.alignmentX = Component.LEFT_ALIGNMENT
+        inputLabel.alignmentX = Component.LEFT_ALIGNMENT
+        exampleLabel.alignmentX = Component.LEFT_ALIGNMENT
+        examplePanel.alignmentX = Component.LEFT_ALIGNMENT
+        settingsFilePathField.alignmentX = Component.LEFT_ALIGNMENT
+
+        val inputPanel = JPanel()
+        inputPanel.layout = BoxLayout(inputPanel, BoxLayout.Y_AXIS)
+        inputPanel.add(inputLabel)
+        inputPanel.add(settingsFilePathField)
+        inputPanel.add(noteLabel)
+        inputPanel.add(exampleLabel)
+        inputPanel.add(examplePanel)
 
         val form = FormBuilder.createFormBuilder()
-            .addLabeledComponent(inputLabel, settingsFilePathField, 1, false)
+            .addComponent(inputPanel)
             .addComponentFillVertically(JPanel(), 0)
             .panel
 
-        panel= JPanel()
-        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
+        return form
+    }
 
-        panel.add(form, 0)
-        panel.add(JBLabel("Note: Default relative path $defaultFileRelativePath"), 1)
+    private fun buildExamplePanel(): JEditorPane {
+        val editorPane = JEditorPane("text/html", getJsonExampleText())
+
+        editorPane.isEditable = false
+        editorPane.border = null
+        editorPane.alignmentX = Component.LEFT_ALIGNMENT
+
+        return editorPane
     }
 
     @get:NotNull
@@ -62,4 +103,11 @@ class AppSettingsComponent {
             false
         )
     }
+
+    private fun getJsonExampleText(): String = """<pre style="padding: 3px 4px;">{
+  "id": "required: API project ID",
+  "key": "required: API project auth key",
+  "agent_email": "optional: organization e-mail",
+  "agent_url": "optional: organization contact page"
+}</pre>""".trimMargin()
 }
